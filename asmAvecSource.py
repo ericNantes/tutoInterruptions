58a7adfe0ce42b697dc0bef44bf07df31c0e6563
ericNantes <eric@geii.eu>
2021-03-22 09:50:22 +0100

    first commit

---
+++ asmAvecSource.py
@@ -0,0 +1,21 @@
+Import("env", "projenv")
+# Ajout de l'option -g aux indicateurs du linker pour obtenir le code source dans le fichier elf.
+env.Append(LINKFLAGS=["-g"])
+
+# Ajout d'un outil dans le menu Custom de PIO
+env.AddCustomTarget(
+    "asm_dump",
+    "$BUILD_DIR/${PROGNAME}.elf",
+    "C:/Users/%username%/.platformio/packages/toolchain-atmelavr/avr/bin/objdump -D -S $BUILD_DIR/${PROGNAME}.elf > $BUILD_DIR/${PROGNAME}.asm",
+    "Dump ELF to ASM",
+    "Dump de firmware.elf vers firmware.asm (source + ASM résultant de la compilation)."
+)
+
+# Fonction de call back pour extraire le code asm compilé avec le code source
+def asmAvecSource(source, target, env):
+    print("Dump firmware.elf ...")
+    env.Execute("C:/Users/%username%/.platformio/packages/toolchain-atmelavr/avr/bin/objdump -D -S $BUILD_DIR/${PROGNAME}.elf > $BUILD_DIR/${PROGNAME}.asm")
+    # do some actions
+
+# Ajout d'une fonction de call back a exécuté lorsque le fichier firmware.elf est créé.
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.elf", asmAvecSource)
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#define PIN_BP 2
+#define DELAI_SANS_REBOND 100
+
+volatile int compteurBP = 0;
+
+void ISR_BP() {
+  static u32 precedenteIT = 0;
+  u32 _millis = millis();
+  if ((_millis - precedenteIT) >= DELAI_SANS_REBOND) {
+    compteurBP++;
+  }
+  precedenteIT = _millis;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_BP, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_BP), ISR_BP, FALLING);
+}
+
+void loop() {
+  Serial.println(compteurBP);
+  Serial.println(compteurBP);
+  delay(3000);
+}
